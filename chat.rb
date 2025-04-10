621480e61ed896006c7384508f36ece0b70d09f5
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-10 20:49:14 +0000

    part 1

--- chat.rb
+++ chat.rb
@@ -4,4 +4,30 @@ require "dotenv/load"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
-pp client
+line_width = 50
+puts "Hello! How can I help you today?"
+puts "-" * line_width
+user_request = gets.chomp
+
+# Prepare Array
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => user_request
+  }
+]
+
+# Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+puts api_response
+puts "-" * line_width
